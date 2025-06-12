584a89b29605e8ff3dab01e4268c2ec4917f1886
Rafael Ferreira <rafa_fcontato@hotmail.com>
2025-06-12 20:35:24 -0300

    Funcao ativar itens no orcamento via url p/ checked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//ativar links no menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -7,4 +8,16 @@ function ativarLink(link) {
         link.classList.add('ativo');
     }
 }
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+//ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(paramentro) {
+    const elemento = document.getElementById(paramentro);
+    if(elemento){
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProdutos);
